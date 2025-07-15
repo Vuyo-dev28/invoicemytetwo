1c8c59c6d2b0d3e44645df58bea269dd7a5b51c2
iFlex Prod. <iflexprodbeats@gmail.com>
2025-07-15 08:30:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 if (!supabaseUrl) {
-  throw new Error("Missing env.SUPABASE_URL")
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL")
 }
 if (!supabaseAnonKey) {
-    throw new Error("Missing env.SUPABASE_ANON_KEY")
+    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY")
 }
 
 
