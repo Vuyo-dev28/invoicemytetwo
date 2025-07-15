24efe80a442603f151b903ef1a32c3212f89aec1
iFlex Prod. <iflexprodbeats@gmail.com>
2025-07-15 08:03:50 +0000

    dont use any cookies:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-import AppLayout from './(app)/layout';
-import DashboardPage from './(app)/page';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -15,9 +13,6 @@ export default async function Home() {
     redirect('/login');
   }
 
-  return (
-    <AppLayout>
-      <DashboardPage />
-    </AppLayout>
-  );
+  // If there is a session, redirect to the dashboard
+  redirect('/invoices');
 }
