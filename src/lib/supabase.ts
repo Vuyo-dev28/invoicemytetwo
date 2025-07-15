5dfc623bcfb2bdc1ffe640ef98fd9a821c59f4b2
iFlex Prod. <iflexprodbeats@gmail.com>
2025-07-15 08:27:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,34 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
 if (!supabaseUrl) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL")
+  throw new Error("Missing env.SUPABASE_URL")
 }
 if (!supabaseAnonKey) {
-    throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY")
+    throw new Error("Missing env.SUPABASE_ANON_KEY")
 }
 
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        persistSession: true,
-        autoRefreshToken: true,
-        detectSessionInUrl: true,
-        storage: {
-            getItem: (key) => {
-                if (typeof window === 'undefined') return null;
-                return window.localStorage.getItem(key);
-            },
-            setItem: (key, value) => {
-                if (typeof window === 'undefined') return;
-                window.localStorage.setItem(key, value);
-            },
-            removeItem: (key) => {
-                if (typeof window === 'undefined') return;
-                window.localStorage.removeItem(key);
-            }
-        }
-    }
-})
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
