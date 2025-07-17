07daa7f5bd89efc5ce99fbbdda68f0494ab62264
New Username <newemail@example.com>
2025-07-17 18:45:27 +0200

    anon key change

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
 
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL and anonymous key must be provided.");
