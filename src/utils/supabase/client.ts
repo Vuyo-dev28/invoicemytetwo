2656e8545a20f9c5b69aa7f55e80ac57ea8ef47e
New Username <newemail@example.com>
2025-07-17 19:06:55 +0200

    loggin

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,17 +1,32 @@
-// import { createBrowserClient } from "@supabase/ssr";
+// // import { createBrowserClient } from "@supabase/ssr";
 
-// export const createClient = () => {
-//   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-//   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
-
-//   // if (!supabaseUrl || !supabaseAnonKey) {
-//   //   throw new Error("Supabase URL and anonymous key must be provided.");
-//   // }
-
-//   // return createBrowserClient(supabaseUrl, supabaseAnonKey);
-// };
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = (supabaseUrl: string, supabaseAnon: string) => {
-  return createBrowserClient(supabaseUrl, supabaseAnon);
+// // export const createClient = () => {
+// //   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// //   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
+
+// //   // if (!supabaseUrl || !supabaseAnonKey) {
+// //   //   throw new Error("Supabase URL and anonymous key must be provided.");
+// //   // }
+
+// //   // return createBrowserClient(supabaseUrl, supabaseAnonKey);
+// // };
+// import { createBrowserClient } from "@supabase/ssr";
+
+// export const createClient = (supabaseUrl: string, supabaseAnon: string) => {
+//   return createBrowserClient(supabaseUrl, supabaseAnon);
+// };
+export const createClient = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
+
+  console.log('Supabase URL:', supabaseUrl);
+  console.log('Supabase Anon Key:', supabaseAnonKey);
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("Supabase URL and anonymous key must be provided.");
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseAnonKey);
 };
