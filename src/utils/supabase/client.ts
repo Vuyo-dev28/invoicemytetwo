1fbdc8a67162c5e084302b16a0b6b0ee0b55c0df
New Username <newemail@example.com>
2025-07-17 20:32:04 +0200

    client

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,11 +1,11 @@
-
 import { createBrowserClient } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const createClient = () =>
-  createBrowserClient(
-    supabaseUrl!,
-    supabaseKey!,
-  );
+  createBrowserClient(supabaseUrl, supabaseAnonKey);
