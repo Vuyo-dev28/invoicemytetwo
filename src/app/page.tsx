e33ecf2a71b6038daf8413c3941aa6f690c8b5c1
iFlex Prod. <iflexprodbeats@gmail.com>
2025-07-18 07:48:57 +0000

    this is how it looks:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from '@/components/ui/button';
 import { Logo } from '@/components/logo';
 import Link from 'next/link';
 import { Input } from '@/components/ui/input';
-import Image from 'next/image';
 
 export default function LandingPage() {
   return (
@@ -42,8 +41,8 @@ export default function LandingPage() {
             </nav>
         </div>
       </header>
-      <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+      <main className="flex-1 flex items-center justify-center">
+        <section className="w-full">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
